c6d52c1f9c66356e7d7af49761d7707337882076
Kyle Rohr-Morey <kylemorey1994@gmail.com>
2024-05-04 17:15:34 -0400

    add password checking for login

--- models/account.model.js
+++ models/account.model.js
@@ -26,10 +26,12 @@ const AccountSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
+// Create virtual password property for validation
 AccountSchema.virtual("cPassword")
   .get(() => this.cPassword)
   .set((value) => (this.cPassword = value));
 
+// Validate password with virtual password
 AccountSchema.pre("validate", function (next) {
   if (this.password !== this.cPassword) {
     this.invalidate("cPassword", "Passwords don't match");
@@ -37,6 +39,7 @@ AccountSchema.pre("validate", function (next) {
   next();
 });
 
+// Validate email is unique
 AccountSchema.pre("validate", async function (next) {
   const takenAccount = await Account.find({
     email: this.email,
@@ -47,6 +50,7 @@ AccountSchema.pre("validate", async function (next) {
   next();
 });
 
+// Hash password on account creation
 AccountSchema.pre("save", function (next) {
   bcrypt.hash(this.password, 10).then((hash) => {
     this.password = hash;
@@ -55,16 +59,32 @@ AccountSchema.pre("save", function (next) {
 });
 
 AccountSchema.post("findOne", async function (result, next) {
-  console.log(this.getFilter(), result);
+  // Check if user exists in db
   if (!result) {
     const newErr = new mongoose.Error.ValidationError(this);
     newErr.addError(
       "username",
       new mongoose.Error.ValidatorError({ message: "Username not found." })
     );
-    next(newErr);
+    return next(newErr);
   }
-  next();
+  // Compare passwords
+  const isCorrectPassword = await bcrypt.compare(
+    this.getQuery()["$or"][1].password,
+    result.password
+  );
+  // Handle password compare
+  if (isCorrectPassword) return next();
+  else {
+    const newErr = new mongoose.Error.ValidationError(this);
+    newErr.addError(
+      "password",
+      new mongoose.Error.ValidatorError({
+        message: "Incorrect password.",
+      })
+    );
+    return next(newErr);
+  }
 });
 
 const Account = mongoose.model("Account", AccountSchema);
